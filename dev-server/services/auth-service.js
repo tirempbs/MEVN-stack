d62e00b319620627f45bb58002c99199c3809150
Tirem Bareno-Sosna <tirempbs@gmail.com>
2019-07-30 13:03:15 -0400

    Finished user sessions

--- dev-server/services/auth-service.js
+++ dev-server/services/auth-service.js
@@ -7,24 +7,36 @@ export function generateJWT(user) {
 
 export function requireLogin(req, res, next) {
   const token = decodeToken(req);
-
   if (!token) {
     return res.status(401).json({ message: 'You must be logged in.' });
   }
-  
   next();
 }
 
 export function decodeToken(req) {
   const token = req.headers.authorization || req.headers['authorization'];
-
   if (!token) {
     return null;
   }
-
   try {
     return jwt.verify(token, process.env.TOKEN_SECRET)
   } catch (error) {
     return null;
   }
+}
+
+export function getUsername(req) {
+  const token = decodeToken(req);
+  if (!token) {
+    return null;
+  }
+  return token.user.username;
+}
+
+export function getUserId(req) {
+  const token = decodeToken(req);
+  if (!token) {
+    return null;
+  }
+  return token.user.id;
 }
